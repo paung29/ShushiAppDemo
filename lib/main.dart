232838d2d74b5bd2010c9597487e4ab37237ea03
paung29 <aikepaung767@gmail.com>
2024-05-06 02:58:07 +0700

    first

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:shushishop/pages/intro_pages.dart';
+
+void main(){
+  runApp(
+    MyApp()
+  );
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: IntroPage(),
+    );
+  }
+}
